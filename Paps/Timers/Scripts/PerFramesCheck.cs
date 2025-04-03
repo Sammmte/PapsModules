1ca514fcfdf9dc42352c3d9bd92305bae3f35d16
Sammmte <santiagoperez.-@hotmail.com>
2025-04-03 19:25:36 -0300

    created timed check for sync and frame timer

---
+++ Paps/Timers/Scripts/PerFramesCheck.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Paps.Timers
+{
+    public class PerFramesCheck<TCheckResult>
+    {
+        private readonly Func<TCheckResult> _check;
+        private readonly FrameTimer _timer = new FrameTimer();
+
+        public int Frames
+        {
+            get => _timer.Frames;
+            set => _timer.Frames = value;
+        }
+
+        private TCheckResult _lastResult;
+
+        public PerFramesCheck(Func<TCheckResult> check)
+        {
+            _check = check;
+        }
+
+        public TCheckResult Check()
+        {
+            if (!_timer.Active)
+            {
+                _timer.Start();
+                _lastResult = _check();
+            }
+
+            return _lastResult;
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
---
+++ Paps/Timers/Scripts/TimedCheck.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Paps.Timers
+{
+    public class TimedCheck<TCheckResult>
+    {
+        private readonly Func<TCheckResult> _check;
+        private readonly SyncTimer _timer = new SyncTimer();
+
+        public float Interval
+        {
+            get => _timer.Interval;
+            set => _timer.Interval = value;
+        }
+
+        private TCheckResult _lastResult;
+
+        public TimedCheck(Func<TCheckResult> check)
+        {
+            _check = check;
+        }
+
+        public TCheckResult Check()
+        {
+            if (!_timer.Active)
+            {
+                _timer.Start();
+                _lastResult = _check();
+            }
+
+            return _lastResult;
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
