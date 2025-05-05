8accb1325b38f5592b0665d93e12e448ac0ef194
Sammmte <santiagoperez.-@hotmail.com>
2025-05-05 12:32:36 -0300

    created timer and check for fixed time steps

---
+++ Paps/Timers/Scripts/FixedTimeStepTimer.cs
@@ -0,0 +1,48 @@
+﻿using Gilzoide.UpdateManager;
+using System;
+
+namespace Paps.Timers
+{
+    public class FixedTimeStepTimer : IFixedUpdatable
+    {
+        public int Steps { get; set; }
+        public bool Loop { get; set; }
+        public event Action<FixedTimeStepTimer> OnTick;
+        public bool Active { get; private set; }
+
+        private float _accumulationFrames;
+
+        public void Start()
+        {
+            if(Active)
+                return;
+            
+            Active = true;
+            _accumulationFrames = 0;
+            this.RegisterInManager();
+        }
+
+        public void Stop()
+        {
+            if(!Active)
+                return;
+            
+            Active = false;
+            this.UnregisterInManager();
+        }
+        
+        public void ManagedFixedUpdate()
+        {
+            _accumulationFrames++;
+
+            if (_accumulationFrames >= Steps)
+            {
+                if(!Loop)
+                    Stop();
+                
+                _accumulationFrames = 0;
+                OnTick?.Invoke(this);
+            }
+        }
+    }
+}
---
+++ Paps/Timers/Scripts/PerFixedTimeStepCheck.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Paps.Timers
+{
+    public class PerFixedTimeStepCheck<TCheckResult>
+    {
+        private readonly Func<TCheckResult> _check;
+        private readonly FixedTimeStepTimer _timer = new FixedTimeStepTimer();
+
+        public int Steps
+        {
+            get => _timer.Steps;
+            set => _timer.Steps = value;
+        }
+
+        public TCheckResult LastResult { get; private set; }
+
+        public PerFixedTimeStepCheck(Func<TCheckResult> check)
+        {
+            _check = check;
+        }
+
+        public TCheckResult Check()
+        {
+            if (!_timer.Active)
+            {
+                _timer.Start();
+                LastResult = _check();
+            }
+
+            return LastResult;
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
