3eb69fd5a99be7af111f1c2236bb26fdf7b07915
Sammmte <santiagoperez.-@hotmail.com>
2025-06-27 13:05:43 -0300

    added unparent component

---
+++ Paps/UnityExtensions/Runtime/Unparent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Paps.UnityExtensions
+{
+    public class Unparent : MonoBehaviour
+    {
+        [SerializeField] private Transform[] _transforms;
+
+        private void Awake()
+        {
+            for (int i = 0; i < _transforms.Length; i++)
+            {
+                _transforms[i].parent = null;
+            }
+            
+            Destroy(this);
+        }
+    }
+}
