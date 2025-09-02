127543d3c5cb45b9daa7addd4c8d8290d9ef4c6f
Sammmte <santiagoperez.-@hotmail.com>
2025-09-02 15:18:01 -0300

    added support for uncategorized value references

--- Paps/ValueReferences/Runtime/Scripts/ValueReference.cs
+++ Paps/ValueReferences/Runtime/Scripts/ValueReference.cs
@@ -31,6 +31,8 @@ namespace Paps.ValueReferences
         public static implicit operator T(ValueReference<T> valueReference) => valueReference.Value;
 
         #if UNITY_EDITOR
+        public const string UNCATEGORIZED_GROUP = "Uncategorized";
+        
         [Button]
         private void PingAsset()
         {
@@ -43,7 +45,18 @@ namespace Paps.ValueReferences
                 .Select(guid => UnityEditor.AssetDatabase.LoadAssetAtPath<ValueReferenceGroupAsset>(UnityEditor.AssetDatabase.GUIDToAssetPath(guid)))
                 .Select(group => group.GetReferencesOfType<T>())
                 .Where(references => references.Length != 0)
-                .SelectMany(references => references.Select(r => (r.Path, r.ValueReferenceAsset)));
+                .SelectMany(references => references.Select(r => (r.Path, r.ValueReferenceAsset)))
+                .ToList();
+
+            var uncategorized = UnityEditor.AssetDatabase.FindAssets($"t:{nameof(ValueReferenceAsset)}")
+                .Select(guid =>
+                    UnityEditor.AssetDatabase.LoadAssetAtPath<ValueReferenceAsset>(
+                        UnityEditor.AssetDatabase.GUIDToAssetPath(guid)))
+                .OfType<ValueReferenceAsset<T>>()
+                .Where(asset => !dropdownItems.Any(tuple => tuple.ValueReferenceAsset == asset))
+                .Select(asset => ($"{UNCATEGORIZED_GROUP}/{asset.name}", asset));
+            
+            dropdownItems.InsertRange(0, uncategorized);
 
             return new DropdownList<ValueReferenceAsset<T>>(dropdownItems);
         }
