5c6cf862b64e2ec6c34ae58e5bfcbf183b1523f6
Sammmte <santiagoperez.-@hotmail.com>
2025-09-03 17:01:11 -0300

    created Vector3ValueReferenceAsset

---
+++ Paps/ValueReferences/Runtime/Scripts/Vector3ValueReferenceAsset.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Paps.ValueReferences
+{
+    [CreateAssetMenu(menuName = BASE_CREATE_ASSET_MENU_PATH + "Vector3")]
+    public class Vector3ValueReferenceAsset : ValueReferenceAsset<Vector3>
+    {
+        [SerializeField] private Vector3 _value;
+
+        protected override Vector3 GetValue() => _value;
+    }
+}
