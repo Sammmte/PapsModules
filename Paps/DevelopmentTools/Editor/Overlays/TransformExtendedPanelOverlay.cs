937a2585acf269fc9bdb3434ce867f74db06fe0c
Sammmte <santiagoperez.-@hotmail.com>
2025-10-16 13:56:16 -0300

    created Transform Extended overlay to see and change transform world space values

---
+++ Paps/DevelopmentTools/Editor/Overlays/TransformExtendedPanelOverlay.cs
@@ -0,0 +1,112 @@
+using Unity.Properties;
+using UnityEditor;
+using UnityEditor.Overlays;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Paps.DevelopmentTools.Editor
+{
+    [Overlay(typeof(SceneView), "Transform Extended", true)]
+    public class TransformExtendedPanelOverlay : Overlay
+    {
+        private const string NO_OBJECT_SELECTED_LABEL = "No object selected";
+
+        private TransformWorldDataSource _worldDataSource;
+        private VisualElement _root;
+        private Label _titleLabel;
+        private Vector3Field _worldPositionField;
+        private Vector3Field _worldRotationEulerField;
+        private Vector3Field _worldScaleField;
+        
+        public override VisualElement CreatePanelContent()
+        {
+            _worldDataSource = new TransformWorldDataSource();
+            _root = new VisualElement()
+            {
+                name = "TransformExtendedContainer",
+                dataSource = _worldDataSource
+            };
+            _titleLabel = new Label();
+            _worldPositionField = new Vector3Field("World Position");
+            _worldRotationEulerField = new Vector3Field("World Rotation");
+            _worldScaleField = new Vector3Field("World Scale");
+            _worldScaleField.SetEnabled(false);
+            
+            _root.Add(_titleLabel);
+            _root.Add(_worldPositionField);
+            _root.Add(_worldRotationEulerField);
+            _root.Add(_worldScaleField);
+
+            UpdatePanelBasedOnSelection();
+
+            Selection.selectionChanged += OnSelectionChanged;
+
+            return _root;
+        }
+
+        public override void OnWillBeDestroyed()
+        {
+            Selection.selectionChanged -= OnSelectionChanged;
+        }
+
+        private void OnSelectionChanged()
+        {
+            UpdatePanelBasedOnSelection();
+        }
+
+        private void UpdatePanelBasedOnSelection()
+        {
+            if (Selection.activeGameObject != null)
+            {
+                BindToObject(Selection.activeGameObject);
+            }
+            else
+            {
+                ShowEmpty();
+            }
+        }
+
+        private void BindToObject(GameObject gameObject)
+        {
+            _worldDataSource.Set(gameObject);
+
+            _titleLabel.text = gameObject.name;
+            
+            _worldPositionField.SetBinding(nameof(_worldPositionField.value), new DataBinding()
+            {
+                dataSourcePath = PropertyPath.FromName(nameof(_worldDataSource.WorldPosition))
+            });
+
+            _worldPositionField.style.display = DisplayStyle.Flex;
+            
+            _worldRotationEulerField.SetBinding(nameof(_worldRotationEulerField.value), new DataBinding()
+            {
+                dataSourcePath = PropertyPath.FromName(nameof(_worldDataSource.WorldRotationEuler))
+            });
+            
+            _worldRotationEulerField.style.display = DisplayStyle.Flex;
+            
+            _worldScaleField.SetBinding(nameof(_worldScaleField.value), new DataBinding()
+            {
+                dataSourcePath = PropertyPath.FromName(nameof(_worldDataSource.WorldScale))
+            });
+            
+            _worldScaleField.style.display = DisplayStyle.Flex;
+        }
+
+        private void ShowEmpty()
+        {
+            _titleLabel.text = NO_OBJECT_SELECTED_LABEL;
+            
+            _worldPositionField.ClearBindings();
+            _worldRotationEulerField.ClearBindings();
+            _worldScaleField.ClearBindings();
+
+            _worldPositionField.style.display = DisplayStyle.None;
+            _worldRotationEulerField.style.display = DisplayStyle.None;
+            _worldScaleField.style.display = DisplayStyle.None;
+            
+            _worldDataSource.Clear();
+        }
+    }
+}
---
+++ Paps/DevelopmentTools/Editor/Overlays/TransformWorldDataSource.cs
@@ -0,0 +1,56 @@
+﻿using Unity.Properties;
+using UnityEngine;
+
+namespace Paps.DevelopmentTools.Editor
+{
+    internal class TransformWorldDataSource
+    {
+        private GameObject _gameObject;
+
+        [CreateProperty]
+        public Vector3 WorldPosition
+        {
+            get => _gameObject?.transform.position ?? Vector3.zero;
+            set
+            {
+                if(_gameObject == null)
+                    return;
+
+                _gameObject.transform.position = value;
+            }
+        }
+
+        [CreateProperty]
+        public Vector3 WorldRotationEuler
+        {
+            get => _gameObject?.transform.rotation.eulerAngles ?? Vector3.zero;
+            set
+            {
+                if (_gameObject == null)
+                    return;
+
+                _gameObject.transform.rotation = Quaternion.Euler(value);
+            }
+        }
+
+        [CreateProperty]
+        public Vector3 WorldScale
+        {
+            get => _gameObject?.transform.lossyScale ?? Vector3.zero;
+            set
+            {
+                
+            }
+        }
+        
+        public void Set(GameObject gameObject)
+        {
+            _gameObject = gameObject;
+        }
+
+        public void Clear()
+        {
+            _gameObject = null;
+        }
+    }
+}
