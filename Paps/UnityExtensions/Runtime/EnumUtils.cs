3279254e3b00117f218395e0d3d5e3bda177d720
Sammmte <santiagoperez.-@hotmail.com>
2025-11-14 12:41:53 -0300

    created EnumUtils class

---
+++ Paps/UnityExtensions/Runtime/EnumUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Paps.UnityExtensions
+{
+    public static class EnumUtils
+    {
+        private static Dictionary<Type, int> _enumData = new Dictionary<Type, int>();
+        
+        public static int GetCountOf<T>() where T : struct, Enum
+        {
+            var type = typeof(T);
+
+            if (_enumData.TryGetValue(type, out int value))
+            {
+                return value;
+            }
+
+            _enumData[type] = Enum.GetValues(type).Length;
+
+            return _enumData[type];
+        }
+    }
+}
