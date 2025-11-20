5cd9b9f7f826aa90b88d8b167f56f09ddb28ba3d
Sammmte <santiagoperez.-@hotmail.com>
2025-11-20 12:18:58 -0300

    simplified broadcast channel. by using delegates directly is now supported that add or removal of listeners while on event call

--- Paps/Broadcasting/Scripts/Runtime/BroadcastChannel.cs
+++ Paps/Broadcasting/Scripts/Runtime/BroadcastChannel.cs
@@ -5,47 +5,25 @@ namespace Paps.Broadcasting
 {
     public class BroadcastChannel
     {
-        private class DelegateList
-        {
-            private List<Action> _voidListeners = new List<Action>();
-
-            public void Add(Action listener)
-            {
-                _voidListeners.Add(listener);
-            }
-
-            public void Remove(Action listener)
-            {
-                _voidListeners.Remove(listener);
-            }
-
-            public void Call()
-            {
-                foreach (var listener in _voidListeners)
-                    listener();
-            }
-        }
+        private abstract class DelegateList { }
 
         private class DelegateList<T> : DelegateList where T : IBroadcastEvent
         {
-            private List<Action<T>> _listeners = new List<Action<T>>();
+            private Action<T> _listeners;
 
             public void Add(Action<T> listener)
             {
-                _listeners.Add(listener);
+                _listeners += listener;
             }
 
             public void Remove(Action<T> listener)
             {
-                _listeners.Remove(listener);
+                _listeners -= listener;
             }
 
             public void Call(T ev)
             {
-                Call();
-
-                foreach(var listener in _listeners)
-                    listener(ev);
+                _listeners.Invoke(ev);
             }
         }
 
@@ -65,32 +43,6 @@ namespace Paps.Broadcasting
             list.Add(listener);
         }
 
-        public void Subscribe<T>(Action listener) where T : IBroadcastEvent
-        {
-            var type = typeof(T);
-
-            if (!_listenersByType.ContainsKey(type))
-                _listenersByType.Add(type, new DelegateList<T>());
-
-            var list = (DelegateList<T>)_listenersByType[type];
-
-            list.Add(listener);
-        }
-
-        public void Subscribe(Type type, Action listener)
-        {
-            if (!_listenersByType.ContainsKey(type))
-            {
-                var newType = typeof(DelegateList<>).MakeGenericType(type);
-                var newDelegateList = (DelegateList)Activator.CreateInstance(newType);
-                _listenersByType.Add(type, newDelegateList);
-            }
-
-            var list = _listenersByType[type];
-
-            list.Add(listener);
-        }
-
         public void Unsubscribe<T>(Action<T> listener) where T : IBroadcastEvent
         {
             var type = typeof(T);
@@ -98,28 +50,6 @@ namespace Paps.Broadcasting
             if(_listenersByType.ContainsKey(type))
             {
                 var list = (DelegateList<T>)_listenersByType[type];
-
-                list.Remove(listener);
-            }
-        }
-
-        public void Unsubscribe<T>(Action listener) where T : IBroadcastEvent
-        {
-            var type = typeof(T);
-
-            if(_listenersByType.ContainsKey(type))
-            {
-                var list = (DelegateList<T>)_listenersByType[type];
-
-                list.Remove(listener);
-            }
-        }
-
-        public void Unsubscribe(Type type, Action listener)
-        {
-            if (_listenersByType.ContainsKey(type))
-            {
-                var list = _listenersByType[type];
 
                 list.Remove(listener);
             }
