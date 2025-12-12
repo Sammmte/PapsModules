335c9df9bbd83a035b4427fae71f5705d46fa910
Sammmte <santiagoperez.-@hotmail.com>
2025-12-12 19:50:49 -0300

    created AllocCounter, courtesy of git amend

---
+++ Paps/UnityExtensions/Runtime/AllocCounter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine.Profiling;
+
+namespace Paps.UnityExtensions
+{
+    public class AllocCounter
+    {
+        private Recorder _recorder;
+
+        public AllocCounter() {
+            _recorder = Recorder.Get("GC.Alloc");
+            _recorder.enabled = false;
+
+#if !UNITY_WEBGL
+            _recorder.FilterToCurrentThread();
+#endif
+
+            _recorder.enabled = true;
+        }
+
+        public int Stop() {
+            if (_recorder == null) throw new InvalidOperationException("AllocCounter was not started.");
+
+            _recorder.enabled = false;
+
+#if !UNITY_WEBGL
+            _recorder.CollectFromAllThreads();
+#endif
+
+            int result = _recorder.sampleBlockCount;
+            _recorder = null;
+            return result;
+        }
+
+        public static int Instrument(Action action) {
+            var counter = new AllocCounter();
+            int allocs;
+
+            try {
+                action();
+            }
+            finally {
+                allocs = counter.Stop();
+            }
+
+            return allocs;
+        }
+    }
+}
