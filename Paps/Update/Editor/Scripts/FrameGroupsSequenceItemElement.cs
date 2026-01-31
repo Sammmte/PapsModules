b6b93698abf74ea1bcfcf7b2fd8115bdaf80a2ae
Sammmte <santiagoperez.-@hotmail.com>
2026-01-31 13:16:45 -0300

    implemented group selection on frame sequence group element that was left un-implemented

--- Paps/Update/Editor/Scripts/FrameGroupsSequenceItemElement.cs
+++ Paps/Update/Editor/Scripts/FrameGroupsSequenceItemElement.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -13,6 +14,8 @@ namespace Paps.Update.Editor
         private Func<int, string[]> _getAvailableGroups;
 
         private SerializedProperty _property;
+
+        public string CurrentGroup => _property.stringValue;
 
         public void Initialize(Func<int, string[]> getAvailableGroups)
         {
@@ -43,10 +46,38 @@ namespace Paps.Update.Editor
         {
             var availableGroups = _getAvailableGroups.Invoke(_frameGroupsSequenceIndex);
 
-            for(int i = 0; i < availableGroups.Length; i++)
+            if(availableGroups.Length == 0)
+                return;
+
+            var menuGroups = availableGroups.Append(CurrentGroup).OrderBy(g => g).ToArray();
+
+            var genericMenu = new GenericMenu();
+
+            for(int i = 0; i < menuGroups.Length; i++)
             {
-                Debug.Log($"AVAILABLE GROUP: {availableGroups[i]}");
+                var group = menuGroups[i];
+
+                if(group == CurrentGroup)
+                {
+                    genericMenu.AddItem(new GUIContent(group), true, () => { });
+                }
+                else
+                {
+                    genericMenu.AddItem(new GUIContent(group), false, OnGroupSelected, group);
+                }
             }
+
+            genericMenu.ShowAsContext();
+        }
+
+        private void OnGroupSelected(object groupObj)
+        {
+            var group = groupObj as string;
+
+            _property.stringValue = group;
+            _property.serializedObject.ApplyModifiedProperties();
+
+            _groupButton.text = group;
         }
     }
 }
