c37df439d39d6d5fb4b634446842662e93fe9c4d
Sammmte <santiagoperez.-@hotmail.com>
2026-03-07 16:03:45 -0300

    added some extensions to level bounds

--- Paps/Levels/Scripts/LevelSetupExtensions.cs
+++ Paps/Levels/Scripts/LevelSetupExtensions.cs
@@ -8,5 +8,9 @@ namespace Paps.Levels
         {
             return LevelManager.Instance.AddLevelBoundComponent<T>(gameObject);
         }
+
+        public static bool DidLoaded(this ILevelBound levelBound) => LevelManager.Instance.DidLoaded(levelBound);
+
+        public static bool DidKickstart(this ILevelBound levelBound) => LevelManager.Instance.DidKickstart(levelBound);
     }
 }
